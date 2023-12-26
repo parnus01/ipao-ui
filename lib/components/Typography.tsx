70a0099c83d4b313103cfa445d538b62b58ba663
Theerapat Honghirun <theerapat.h@arise.tech>
2023-12-26 12:12:55 +0700

    init mantine

--- lib/components/Typography.tsx
+++ lib/components/Typography.tsx
@@ -1,9 +1,21 @@
-import {ReactNode} from "react";
-
+import { ReactNode } from "react";
+import '@mantine/core/styles.css'
+import { createTheme, rem, Text } from "@mantine/core";
+import "./Typography.css"
 interface ITypography {
-    children: ReactNode
+    children: ReactNode,
+    category: string
 }
 
-export const Typography = ({children}: ITypography) => {
-    return <p className={'ipao-typo'}>{children}</p>
+
+
+export const Typography = ({ children, category }: ITypography) => {
+    let input: any = children?.toString();
+    return (
+        <>
+                <div className={category} dangerouslySetInnerHTML={{__html: input }} />
+                <Text fz="lg" lh="lg" component="div">smt</Text>
+        </>
+    )
+
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@mantine/core/styles.css'
+import { MantineProvider, rem } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+    theme={{
+      fontSizes: {
+        xs: rem('16px'),
+        sm: rem('20px'),
+        md: rem('24px'),
+        lg: rem('32px'),
+        xl: rem('40px'),
+      },
+      lineHeights:{
+        xs: '24px',
+        sm: '32px',
+        md: '36px',
+        lg: '48px',
+        xl: '56px',
+      }
+    }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
