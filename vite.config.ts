fc7df22efc78a51d7c5f60960a505ed970a84b5e
Danusorn Taboonyong <osacn_danusorn.taboonyong@krungthai.com>
2024-02-02 09:25:52 +0700

    chore: using react-dom production when build package

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
-import {defineConfig} from 'vite'
-import {resolve} from 'path'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import path from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import path from "path";
 
-export default defineConfig({
-    plugins: [react(), dts({include: ['lib']})], 
-    build: {
-        copyPublicDir: false,
-        lib: {
-            entry: resolve(__dirname, 'lib/main.ts'),
-            formats: ['es']
-        },
-        rollupOptions: {
-            external: ['react', 'react/jsx-runtime'],
-        }
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), dts({ include: ["lib"] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
     },
-    resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "./lib")
-        }
-      }
-})
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(mode),
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./lib"),
+    },
+  },
+}));
